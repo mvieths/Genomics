e9a792306e7d28873d8dd008faf63c0673dc2f64
Michael Vieths <mvieths@gmail.com>
2013-10-30 11:31:40 -0500

    Handle 'N' in sequence

--- src/mvieths/HMM/Viterbi.java
+++ src/mvieths/HMM/Viterbi.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Stack;
 
@@ -27,7 +28,7 @@ public class Viterbi {
     // final static double q = 0.94;
 
     static char[]                      observations       =
-                                                              { 'A', 'C', 'G', 'T' };
+                                                          { 'A', 'C', 'G', 'T', 'N' };
 
     /*
      * A + indicates a probability within a CpG island, - indicates out of CpG island
@@ -35,30 +36,31 @@ public class Viterbi {
      * e.g. A+ to A- represents a transition from an A inside a CpG island to an A outside of a CpG island
      */
     static String[]                    states             =
-                                                              { "A+", "C+", "G+", "T+", "A-", "C-", "G-", "T-" };
+                                                          { "A+", "C+", "G+", "T+", "A-", "C-", "G-", "T-" };
 
     static double[][]                  transitionMatrix   =
-                                                              {
-                                                                  // A+ C+ G+ T+ A- C- G- T-
-                                                                  { 0.180 * p, 0.274 * p, 0.426 * p, 0.120 * p, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4 }, // A+
-                                                                  { 0.171 * p, 0.368 * p, 0.274 * p, 0.188 * p, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4 }, // C+
-                                                                  { 0.161 * p, 0.339 * p, 0.375 * p, 0.125 * p, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4 }, // G+
-                                                                  { 0.079 * p, 0.355 * p, 0.384 * p, 0.182 * p, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4 }, // T+
-                                                                  { (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, 0.300 * q, 0.205 * q, 0.285 * q, 0.210 * q }, // A-
-                                                                  { (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, 0.322 * q, 0.298 * q, 0.078 * q, 0.302 * q }, // C-
-                                                                  { (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, 0.248 * q, 0.246 * q, 0.298 * q, 0.208 * q }, // G-
-                                                                  { (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, 0.177 * q, 0.239 * q, 0.292 * q, 0.292 * q } // T-
-                                                              };
+                                                          {
+                                                          // A+ C+ G+ T+ A- C- G- T-
+            { 0.180 * p, 0.274 * p, 0.426 * p, 0.120 * p, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4 }, // A+
+            { 0.171 * p, 0.368 * p, 0.274 * p, 0.188 * p, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4 }, // C+
+            { 0.161 * p, 0.339 * p, 0.375 * p, 0.125 * p, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4 }, // G+
+            { 0.079 * p, 0.355 * p, 0.384 * p, 0.182 * p, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4, (1 - p) / 4 }, // T+
+            { (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, 0.300 * q, 0.205 * q, 0.285 * q, 0.210 * q }, // A-
+            { (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, 0.322 * q, 0.298 * q, 0.078 * q, 0.302 * q }, // C-
+            { (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, 0.248 * q, 0.246 * q, 0.298 * q, 0.208 * q }, // G-
+            { (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, (1 - q) / 4, 0.177 * q, 0.239 * q, 0.292 * q, 0.292 * q } // T-
+                                                          };
 
     // Emission probabilities are 1 when it's the same base, 0 otherwise
     static double[][]                  emissionMatrix     =
-                                                              {
-                                                                  // A+ C+ G+ T+ A- C- G- T-
-                                                                  { 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0 }, // A
-                                                                  { 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0 }, // C
-                                                                  { 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0 }, // G
-                                                                  { 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0 } // T
-                                                              };
+                                                          {
+                                                          // A+ C+ G+ T+ A- C- G- T-
+            { 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0 }, // A
+            { 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0 }, // C
+            { 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0 }, // G
+            { 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0 }, // T
+            { 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 }   // N
+                                                          };
 
     // Reverse mapping from string to integer for looking up a state
     static HashMap<String, Integer>    stateMap;
@@ -66,7 +68,10 @@ public class Viterbi {
     static HashMap<Character, Integer> obsMap;
 
     static double[]                    initialProbability =
-                                                              { 1.0 / 8.0, 1.0 / 8.0, 1.0 / 8.0, 1.0 / 8.0, 1.0 / 8.0, 1.0 / 8.0, 1.0 / 8.0, 1.0 / 8.0 };
+                                                          { 1.0 / 8.0, 1.0 / 8.0, 1.0 / 8.0, 1.0 / 8.0, 1.0 / 8.0,
+                                                          1.0 / 8.0, 1.0 / 8.0, 1.0 / 8.0 };
+
+    static long                        startTime;
 
     /**
      * @param args
@@ -85,6 +90,7 @@ public class Viterbi {
         }
 
         String filename = args[0];
+        startTime = Calendar.getInstance().getTimeInMillis();
 
         String toySequence1 = parseFASTA(filename);
         System.out.println("sequence is " + toySequence1.length() + " long");
@@ -101,7 +107,8 @@ public class Viterbi {
             // Probability is 0 if emission probability is 0 (to avoid Math.log issues)
             if (emissionMatrix[obsMap.get(firstChar)][i] != 0.0) {
                 probabilityTable[i][0] = Math.log(initialProbability[i] * emissionMatrix[obsMap.get(firstChar)][i]);
-            } else {
+            }
+            else {
                 probabilityTable[i][0] = 0;
             }
             stateTable[i][0] = 0;
@@ -120,8 +127,14 @@ public class Viterbi {
                 for (int k = 0; k < states.length; k++) {
                     double prob = Double.NEGATIVE_INFINITY;
                     // Math.log(0) comes out as negative infinity, so only get the log and sum the probability if it's non-zero
-                    if (emissionMatrix[obsMap.get(thisChar)][k] != 0.0) {
-                        prob = probabilityTable[k][seqPos - 1] + Math.log(transitionMatrix[k][state]) + Math.log(emissionMatrix[obsMap.get(thisChar)][k]);
+                    if (thisChar == 'N') {
+                        // 'N' stands for any, so provide an equal probability of any state
+                        prob = probabilityTable[k][seqPos - 1] + Math.log(1.0 / 8.0)
+                                + Math.log(emissionMatrix[obsMap.get(thisChar)][k]);
+                    }
+                    else if (emissionMatrix[obsMap.get(thisChar)][k] != 0.0) {
+                        prob = probabilityTable[k][seqPos - 1] + Math.log(transitionMatrix[k][state])
+                                + Math.log(emissionMatrix[obsMap.get(thisChar)][k]);
                     }
 
                     if (prob > maxProbability) {
@@ -177,7 +190,8 @@ public class Viterbi {
                 islands.add(island);
                 island = new CpGIsland();
                 // System.out.println("Left island at " + i);
-            } else if (!inIsland && state < 4) {
+            }
+            else if (!inIsland && state < 4) {
                 inIsland = true;
                 island.setStart(i);
                 // System.out.println("Entered island at " + i);
@@ -185,9 +199,12 @@ public class Viterbi {
             i++;
         }
 
-        for (CpGIsland next : islands) {
-            System.out.println("There's an island between " + next.getStart() + " and " + next.getEnd());
-        }
+        //        for (CpGIsland next : islands) {
+        //            System.out.println("There's an island between " + next.getStart() + " and " + next.getEnd());
+        //        }
+        System.out.println("Total islands: " + islands.size());
+        long endTime = Calendar.getInstance().getTimeInMillis();
+        System.out.println("Finished in " + (endTime - startTime) / 1000 + " seconds");
     }
 
     /**
@@ -209,7 +226,8 @@ public class Viterbi {
                     sequence += line.replaceAll("\\s+", "");
                 }
             }
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             System.out.println("Failed to read file " + filename);
             e.printStackTrace();
         }
